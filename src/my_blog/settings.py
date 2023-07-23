285188acaa8fc7b8995b19951fbb3ac016b1f9b7
mahmoudsalemabdelrahman <mahmoud19salem@gmail.com>
2023-07-23 21:49:28 +0300

    make static folder for all blog

--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,8 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+#if you want make Static folder for all project you can use this code .
 STATIC_URL = 'static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR,"static"),
+    '/home/mahmoud/Desktop/bloger/src/static',
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
