e939c837c2bb71c6adcefa7908ffd3d38edcfffe
mahmoudsalemabdelrahman <mahmoud19salem@gmail.com>
2023-07-24 22:29:00 +0300

    make about page

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home' ),
+    path('about/', views.about, name='about'),
 ]#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+posts =[ 
+    {
+        'title':'التدوينه الاولي',
+        'content':'نص التدوينه الاولي تجريببي',
+        'post_date':'15-3-2019',
+        'author':'Mahmoud Salem',
+    },
+        {
+        'title':'التدوينه الثانيه',
+        'content':'نص التدوينه الثانيه تجريببي',
+        'post_date':'28-3-2019',
+        'author':'mohamed Ali',
+    },
+        {
+        'title':'التدوينه الثالثه',
+        'content':'نص التدوينه الثالثه تجريببي',
+        'post_date':'15-3-2019',
+        'author':'Ali Salem',
+    },
+        {
+        'title':'التدوينه الرابعه',
+        'content':'نص التدوينه الرابعه تجريببي',
+        'post_date':'15-3-2019',
+        'author':'Mazen Ibrahem',
+    }
+]
 def home(request):
-    return render(request ,'blog/index.html', {'title':'Home'})
+    context ={
+        'title':'الصفحه الرئيسيه',
+        'posts':posts
+    }
+    return render(request ,'blog/index.html',context)
+
+def about(request):
+        return render(request, 'blog/about.html' ,{'title':'من أنا'})
