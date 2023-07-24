c9eae7067311e49e09babaacf1b148e2f3ace4ff
mahmoudsalemabdelrahman <mahmoud19salem@gmail.com>
2023-07-25 00:31:09 +0300

    make models db

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,37 +1,14 @@
 from django.shortcuts import render
+from blog.models import Post
+
 
 # Create your views here.
 
-posts =[ 
-    {
-        'title':'التدوينه الاولي',
-        'content':'نص التدوينه الاولي تجريببي',
-        'post_date':'15-3-2019',
-        'author':'Mahmoud Salem',
-    },
-        {
-        'title':'التدوينه الثانيه',
-        'content':'نص التدوينه الثانيه تجريببي',
-        'post_date':'28-3-2019',
-        'author':'mohamed Ali',
-    },
-        {
-        'title':'التدوينه الثالثه',
-        'content':'نص التدوينه الثالثه تجريببي',
-        'post_date':'15-3-2019',
-        'author':'Ali Salem',
-    },
-        {
-        'title':'التدوينه الرابعه',
-        'content':'نص التدوينه الرابعه تجريببي',
-        'post_date':'15-3-2019',
-        'author':'Mazen Ibrahem',
-    }
-]
+
 def home(request):
     context ={
         'title':'الصفحه الرئيسيه',
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request ,'blog/index.html',context)
 
