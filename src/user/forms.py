3a6cc4b396b75efc54753dd04cdfd19a189b54a4
Mahmoud Salem <95503402+mahmoudsalemabdelrahman@users.noreply.github.com>
2023-08-22 10:46:30 +0000

    create page login and logout

--- src/user/forms.py
+++ src/user/forms.py
@@ -12,3 +12,17 @@ class UserCreationForms(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] != cd['password2']:
+            raise forms.ValidationError('كلمه المرور خاطئه')
+        return cd['password2']
+    
+
+    def clean_username(self):
+        cd = self.cleaned_data
+        if User.objects.filter(username=cd['username']).exists():
+            raise forms.ValidationError('يوجد مستخم مسجل بهذا الاسم')
+        return cd['username']
--- src/user/views.py
+++ src/user/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserCreationForms
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    form = UserCreationForms()
+    if request.method == 'POST':
+        form = UserCreationForms(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.success(request, f'تهانينا{username} لقد تم التسجيل بنجاح')
+            return redirect('home')
+    else:
+        form = UserCreationForms()
     return render(request, 'user/register.html', {
         'title': 'التسجيل',
         'form':form,
